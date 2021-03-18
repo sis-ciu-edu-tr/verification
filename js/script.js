dd409aad4a1571ba538f17b52aa70c4dc2512a37
Omar <chamuromar1@gmail.com>
2021-03-18 21:05:32 +0200

    ffff

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// var pass = prompt("Enter the Verification Code:", "");
+// if (pass == null)  window.location = "../wrong.html";
+// else if (pass == "5Ws14Wqa")  
+//       window.location = "../index.html";
+// else  window.location = "../wrong.html";
+
